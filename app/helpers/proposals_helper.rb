1a11a9ca64eed4e01db71124c177e12a97872410
BSorbus <BSorbus@gmail.com>
2020-01-20 01:59:18 +0100

    Remove additional PitTeryt II

--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -40,24 +40,4 @@ module ProposalsHelper
     t("proposals.status_id_#{data.proposal_status_id}_name")
   end
 
-  def proposal_wizard_progress_bar
-    content_tag(:section, class: "content") do
-      content_tag(:div, class: "navigator") do
-        content_tag(:ol) do
-          wizard_steps.collect do |every_step|
-            class_str = "unfinished"
-            class_str = "current"  if every_step == step
-            class_str = "finished" if past_step?(every_step)
-            concat(
-              content_tag(:li, class: class_str) do
-                #link_to I18n.t(every_step), wizard_path(every_step)
-                link_to every_step, wizard_path(every_step)
-              end 
-            )   
-          end 
-        end 
-      end
-    end
-  end 
-
 end
