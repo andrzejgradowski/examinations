58b1f6d85ce4d9ab66639f500323799589f8f04d
BSorbus <BSorbus@gmail.com>
2020-02-10 13:04:17 +0100

    Whenever as rake job

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,6 +8,12 @@ set :bundle_command, "/home/deploy/.rbenv/shims/bundle exec"
 set :environment, :production
 set :output, "#{Rails.root}/log/cron_log.log"
 
+# every '00 10 * * *' do
+#   runner "Proposal.send_reminders"
+# end
+
 every '00 10 * * *' do
-  runner "Proposal.send_reminders"
+  rake "cronjobs:proposal_reminders"
 end
+
+
