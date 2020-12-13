e14d1a6e0ca7cd540604cc707f05ed627da5250f
BSorbus <BSorbus@gmail.com>
2020-12-13 22:29:04 +0100

    again ...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,18 +16,18 @@ class ApplicationController < ActionController::Base
       I18n.locale = params[:locale] || I18n.default_locale
     end
 
-  def html_request?
-    request.format.html?
-  end
+    def html_request?
+      request.format.html?
+    end
 
-  def action_is_static_pages_home_alert?
-    params[:controller] == 'static_pages' && params[:action] == 'home_alert'
-  end
+    def action_is_static_pages_home_alert?
+      params[:controller] == 'static_pages' && params[:action] == 'home_alert'
+    end
 
-  def check_browser
-    if browser.ie?
-      redirect_to static_pages_home_alert_path
-    end unless action_is_static_pages_home_alert?
-  end
+    def check_browser
+      if browser.ie?
+        redirect_to static_pages_home_alert_path
+      end unless action_is_static_pages_home_alert?
+    end
 
 end
